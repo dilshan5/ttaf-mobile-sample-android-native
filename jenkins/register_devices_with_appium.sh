f4a4f0182ff4528b0285379cfde5080757c851ce
Dilshan F <dilshan.fernando@sysco.com>
2020-12-16 10:38:08 +0530

    Add reference documents

--- jenkins/register_devices_with_appium.sh
+++ jenkins/register_devices_with_appium.sh
@@ -3,6 +3,8 @@
 #  ============= Execute on Ubuntu/Linux Machine ===============================
 #  Used for Start the Appium server
 #  coded by Dilshan fernando
+# http://appium.io/docs/en/writing-running-appium/server-args/
+# http://appium.io/docs/en/writing-running-appium/caps/
 #  ============================================
 
-appium -a 127.0.0.1 -p 1234 -bp 2345 --udid gtest1:5554 --chromedriver-port 5645  --session-override --default-capabilities "{\"systemPort\":8200}"
+appium -a 127.0.0.1 -p 1234 -bp 2345 --udid gtest1:5554 --chromedriver-port 5645 --default-capabilities "{\"systemPort\":8200}"
